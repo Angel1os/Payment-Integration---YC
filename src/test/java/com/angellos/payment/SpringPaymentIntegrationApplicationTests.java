d02ff33d1004a435f1cc92ca4c0d6bcb5296b5d7
Angel1os <101555514+Angel1os@users.noreply.github.com>
2024-05-09 00:11:53 +0000

    Project setup

---
+++ src/test/java/com/angellos/payment/SpringPaymentIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angellos.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaymentIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
